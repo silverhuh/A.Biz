3efeb11d2cfcad180448494e3d81d0343989cc2a
silverhuh <hujunni@naver.com>
2026-05-11 16:36:56 +0900

    Update rules.py

--- app/rules.py
+++ app/rules.py
@@ -6,8 +6,9 @@
 # 채널 ID 정의
 # --------------------------------------------------------
 TM_SERVICEDEVTEAM_DEV_BIZ = "C0B12JP7ARW"   # 서비스개발팀 dev biz 채널
-TEST_ALERT_CH = "C092DJVHVPY"                # Test 채널
-TEST_ERROR_STG_TEST = "C0B2RGH80M9"
+ERRORDISPATCHER_HUH = "C092DJVHVPY"         # Test 수신 채널
+BIZ_BOT_TEST = "C0B2RGH80M9"                # Test 발신 채널
+REPORT_API_ALERT_DEV = "C0AV98LHR8W"        # 비즈 리포트 발신 채널
 
 # --------------------------------------------------------
 # 멘션 ID 정의
@@ -25,29 +26,42 @@ RULES = [
     # 1회 이상 감지 시, Test 채널에 알림 발송
     # ----------------------------------------------------
     {
-        "name": "BIZ_TEST",
+        "name": "BIZ_TEST1",
         "channel": TM_SERVICEDEVTEAM_DEV_BIZ,    # A채널: 서비스개발팀 dev biz
-        "keyword": "Class",                       # 감지 키워드
-        "threshold": 1,                           # 1회 이상 감지 시
+        "keyword": "Class",                      # 감지 키워드
+        "threshold": 1,                          # 1회 이상 감지 시
         "notify": [
             {
-                "channel": TEST_ALERT_CH,         # B채널: Test
+                "channel": ERRORDISPATCHER_HUH,         # B채널: Test 수신 채널
                 "text": f"Test 알림 {MENTION_HEO}",  # @허은석 멘션
-                "include_log": False,             # 원본 로그 미포함
+                "include_log": True,             # 원본 로그 포함
             },
         ],
     },
     {
         "name": "BIZ_TEST2",
-        "channel": TEST_ERROR_STG_TEST,    # A채널: 서비스개발팀 dev biz
-        "keyword": "Class",                       # 감지 키워드
-        "threshold": 1,                           # 1회 이상 감지 시
+        "channel": REPORT_API_ALERT_DEV,    # A채널: 비즈 리포트 발신 채널
+        "keyword": "400",                   # 감지 키워드
+        "threshold": 1,                     # 1회 이상 감지 시
         "notify": [
             {
-                "channel": TEST_ALERT_CH,         # B채널: Test
+                "channel": ERRORDISPATCHER_HUH,      # B채널: Test 수신 채널
                 "text": f"Test 알림 {MENTION_HEO}",  # @허은석 멘션
-                "include_log": False,             # 원본 로그 미포함
+                "include_log": True,                # 원본 로그 포함
             },
         ],
     },    
+    {
+        "name": "BIZ_TEST3",
+        "channel": BIZ_BOT_TEST,            # A채널: Test 발신 채널
+        "keyword": "400",                   # 감지 키워드
+        "threshold": 1,                     # 1회 이상 감지 시
+        "notify": [
+            {
+                "channel": ERRORDISPATCHER_HUH,      # B채널: Test 수신 채널
+                "text": f"Test 알림 {MENTION_HEO}",  # @허은석 멘션
+                "include_log": True,                # 원본 로그 포함
+            },
+        ],
+    },        
 ]
